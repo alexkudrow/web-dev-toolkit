98894cf5cc058c960130ff6fb011f7ceb40788fe
Alexey Kudrow <alexey.kudrow@gmail.com>
2020-03-04 02:02:33 +0700

    Add --base-href and --no-root-urls arguments to html task

--- tasks/html.js
+++ tasks/html.js
@@ -7,9 +7,11 @@ const plumber       = require('gulp-plumber');
 const notify        = require('gulp-notify');
 const pug           = require('gulp-pug');
 const prettyHtml    = require('gulp-pretty-html');
+const replace       = require('gulp-replace');
 const gulpIf        = require('gulp-if');
 const mode          = require('gulp-mode')({ modes: modes });
 const revReplace    = require('gulp-rev-replace');
+const combine       = require('stream-combiner2').obj;
 
 module.exports = function(options) {
     let currentMode;
@@ -21,6 +23,11 @@ module.exports = function(options) {
             break;
         }
     }
+
+    const baseHref = (process.argv.find(arg => /^--base-href/.test(arg)) || "")
+        .split('=')[1];
+
+    const noRootUrls = process.argv.includes('--no-root-urls') || !!baseHref;
 
     return function() {
         return gulp.src(options.src)
@@ -36,6 +43,11 @@ module.exports = function(options) {
                     'environment': currentMode,
                 },
             }))
+            .pipe(gulpIf(noRootUrls, combine(
+                replace(' href="/', ' href="'),
+                replace(' src="/', ' src="'),
+            )))
+            .pipe(gulpIf(!!baseHref, replace("</head>", `<base href="${baseHref}"></head>`)))
             .pipe(prettyHtml())
             .pipe(gulpIf(!mode.development(), revReplace({
                 manifest: gulp.src(options.revFile, { allowEmpty: true })
