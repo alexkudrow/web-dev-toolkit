8ef478bc3f0676e249a2fdceb75d05549286df61
Alexey Kudrow <alexey.kudrow@gmail.com>
2020-03-03 21:21:19 +0700

    Refactor images task and add modes list options in package.json

--- tasks/images.js
+++ tasks/images.js
@@ -1,43 +1,41 @@
 'use strict';
 
-var gulp = require('gulp');
-var $ = require('gulp-load-plugins')({
-    pattern: '*',
-    overridePattern: false,
-    rename: {
-        'gulp-mode': 'initMode',
-    },
-});
+const modes = require('../package.json').modes;
 
-$.mode = $.initMode();
-
-var combine = require('stream-combiner2').obj;
+const gulp              = require('gulp');
+const plumber           = require('gulp-plumber');
+const notify            = require('gulp-notify');
+const newer             = require('gulp-newer');
+const gulpIf            = require('gulp-if');
+const mode              = require('gulp-mode')({ modes: modes });
+const imagemin          = require('gulp-imagemin');
+const imageminMozjpeg   = require('imagemin-mozjpeg');
+const imageminPngquant  = require('imagemin-pngquant');
+const rev               = require('gulp-rev');
+const combine           = require('stream-combiner2').obj;
 
 module.exports = function(options) {
 
     return function() {
         return gulp.src(options.src)
-            .pipe($.plumber({
-                errorHandler: $.notify.onError(function(error) {
-                    return {
-                        title:   options.taskName,
-                        message: error.message,
-                    }
-                }),
+            .pipe(plumber({
+                errorHandler: notify.onError((error) => ({
+                    title:   options.taskName,
+                    message: error.message,
+                })),
             }))
-            .pipe($.newer(options.dest))
-            .pipe($.mode.production(
-                    $.imagemin([
-                    $.imagemin.gifsicle({"interlaced": true}),
-                    $.imageminMozjpeg({"quality": 90, "progressive": true}),
-                    $.imageminPngquant({"quality": 90}),
-                    $.imagemin.svgo()
+            .pipe(newer(options.dest))
+            .pipe(gulpIf(!mode.development(), imagemin([
+                    imagemin.gifsicle({ interlaced: true }),
+                    imageminMozjpeg({ quality: 90, progressive: true }),
+                    imageminPngquant({ quality: 90 }),
+                    imagemin.svgo()
                 ])
             ))
-            .pipe($.mode.production($.rev()))
+            .pipe(gulpIf(!mode.development(), rev()))
             .pipe(gulp.dest(options.dest))
-            .pipe($.mode.production(combine(
-                $.rev.manifest(options.revFile),
+            .pipe(gulpIf(!mode.development(), combine(
+                rev.manifest(options.revFile),
                 gulp.dest(options.tmp)
             )));
     }
